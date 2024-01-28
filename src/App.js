68d34aad678b0d570ffdaca185dedc7bce4cf491
HAZCHEM234 <149445613+HAZCHEM234@users.noreply.github.com>
2024-01-29 10:15:03 +1300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Fronts from './component/Fronts';
+import New from './component/New';
+import Shop from './component/Shop';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Support from './component/Support'
+import Connect from './component/Connect';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Fronts/>
+      <New/>
+      <Shop/>
+      <Support/>
+      <Connect/>
     </div>
   );
 }
